e92eb06d62fc7544d30b6fa669007455c7a29b56
Andrey <samaromku@mail.ru>
2017-12-18 00:32:13 +0300

    сделал нотифаи из консоли

--- app/src/main/java/ru/savchenko/andrey/smssender/MainActivity.kt
+++ app/src/main/java/ru/savchenko/andrey/smssender/MainActivity.kt
@@ -5,21 +5,34 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.telephony.SmsManager
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
 import io.victoralbertos.rx2_permissions_result.RxPermissionsResult
 
 
 class MainActivity : AppCompatActivity() {
+    val PERMISSIONS_STORAGE = arrayOf<String>(Manifest.permission.SEND_SMS)
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i(TAG, FirebaseInstanceId.getInstance().getToken())
+
 //        sendSMS("89189930667", "test message for nice guy")
-        requestPermissions()
+//
+    }
+
+    private fun sendMessage(){
+        if (android.os.Build.VERSION.SDK_INT >=23) {
+            requestPermissions()
+        }else{
+            sendSMS("+79910010987", "test message for nice guy")
+        }
     }
 
     private fun requestPermissions() {
-        val PERMISSIONS_STORAGE = arrayOf<String>(Manifest.permission.SEND_SMS)
-
         RxPermissionsResult.on(this).requestPermissions(*PERMISSIONS_STORAGE)
                 .subscribe({result -> result.targetUI()
                         .showPermissionStatus(result.grantResults())})
@@ -28,7 +41,7 @@ class MainActivity : AppCompatActivity() {
     private fun showPermissionStatus(grantResults: IntArray) {
         val granted = grantResults[0] == PackageManager.PERMISSION_GRANTED
         if (granted) {
-            sendSMS("89189930667", "test message for nice guy")
+            sendSMS("+79910010987", "test message for nice guy")
         }
     }
 
---
+++ app/src/main/java/ru/savchenko/andrey/smssender/service/NotifyService.kt
@@ -0,0 +1,123 @@
+package ru.savchenko.andrey.smssender.service
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.support.v4.app.NotificationCompat
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import ru.savchenko.andrey.smssender.MainActivity
+import ru.savchenko.andrey.smssender.R
+
+
+/**
+ * Created by Andrey on 17.12.2017.
+ */
+class NotifyService : FirebaseMessagingService() {
+    private val TAG = "MyFirebaseMsgService"
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        // [START_EXCLUDE]
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        // [END_EXCLUDE]
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage!!.from!!)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.size > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+            Log.i(TAG, "data text" + remoteMessage.data["text"])
+            //
+            //            if (/* Check if data needs to be processed by long running job */ true) {
+            //                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+            //                scheduleJob();
+            //            } else {
+            //                // Handle message within 10 seconds
+            //                handleNow();
+            //            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body!!)
+            Log.i(TAG, remoteMessage.data["text"] + "text from data")
+            sendNotification(remoteMessage.notification!!.body!!)
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+    // [END receive_message]
+
+    /**
+     * Schedule a job using FirebaseJobDispatcher.
+     */
+    private fun scheduleJob() {
+        // [START dispatch_job]
+        println("start sheduled")
+        //        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
+        //        Job myJob = dispatcher.newJobBuilder()
+        //                .setService(MyJobService.class)
+        //                .setTag("my-job-tag")
+        //                .build();
+        //        dispatcher.schedule(myJob);
+        // [END dispatch_job]
+    }
+
+    /**
+     * Handle time allotted to BroadcastReceivers.
+     */
+    private fun handleNow() {
+        Log.d(TAG, "Short lived task is done.")
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private fun sendNotification(messageBody: String) {
+        println(messageBody)
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT)
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+                //.setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setSmallIcon(R.drawable.ic_mes)
+                .setContentTitle("FCM Message")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setVibrate(longArrayOf(1000, 1000))
+                .setLights(Color.WHITE, 3000, 3000)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+}
